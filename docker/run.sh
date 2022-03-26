51c4556d4126a1e3363f26be5f2a95f87572d384
angelocorreia27 <angelocorreia27@hotmail.com>
2022-03-26 14:31:31 -0100

    fix run issue

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-cd /var/www
+cd /var/www/public
 
 # Deployment steps
 
-npm install && composer install --optimize-autoloader --no-dev
+npm install && composer update && composer install
 npm start
 
 /usr/bin/supervisord -c /etc/supervisord.conf
