1bce6d4bfba1fbe8640f25fc2c64f2f6906981d3
angelocorreia27 <angelocorreia27@hotmail.com>
2022-04-12 16:41:57 -0100

    a

--- docker/run.sh
+++ docker/run.sh
@@ -7,7 +7,7 @@ bash ./docker/generateConfig.sh /var/www/public/config.php
 
 npm install 
 composer update
-composer install --optimize-autoloader --no-dev
+composer install #--optimize-autoloader --no-dev
 #npm start
 
 /usr/bin/supervisord -c /etc/supervisord.conf
