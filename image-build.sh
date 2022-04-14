367667a8a232161c4e9f19a14742ba7a9dcc74dd
angelocorreia27 <angelocorreia27@hotmail.com>
2022-04-14 13:52:37 -0100

    debud 2

--- image-build.sh
+++ image-build.sh
@@ -1,21 +1,21 @@
 sudo -v
 
 echo "Docker login"
-docker login
+#docker login
 
 echo "Building image !"
 
-docker build . -t local/easyappointments:local
+#docker build . -t local/easyappointments:local
 
-echo "Saving image tar!"
+#echo "Saving image tar!"
 docker save local/easyappointments:local > nebua-easyappointments.tar
 
 # Import to microk8s
 echo "Importing image to microk8s!"
 sudo microk8s ctr image import nebua-easyappointments.tar
 
-echo "Remove File tar!"
-sudo rm -r nebua-easyappointments.tar
-echo "Done!"
+#echo "Remove File tar!"
+#sudo rm -r nebua-easyappointments.tar
+#echo "Done!"
 
 #docker image push nebua/easyappointments:latest
