85321982b2bc1dfed8a678670260eedea7c1e8bd
angelocorreia27 <angelocorreia27@hotmail.com>
2022-04-16 08:03:09 -0100

    debug 16

--- docker/run.sh
+++ docker/run.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
-cd /var/www/public
+# cd /var/www/public
 
-# Deployment steps
-bash ./docker/generateConfig.sh /var/www/public/config.php
+# # Deployment steps
+# bash ./docker/generateConfig.sh /var/www/public/config.php
 
-npm install 
-composer update
-composer install #--optimize-autoloader --no-dev
-#npm start
+# npm install 
+# composer update
+# composer install #--optimize-autoloader --no-dev
+# #npm start
 
-/usr/bin/supervisord -c /etc/supervisord.conf
+# /usr/bin/supervisord -c /etc/supervisord.conf
+
+ # Deployment steps
+ bash ./docker/generateConfig.sh /var/www/public/config.php
+
+ npm install 
+# composer update
+ composer install #--optimize-autoloader --no-dev
+# #npm start
+
+# Update nginx to match worker_processes to no. of cpu's
+procs=$(cat /proc/cpuinfo | grep processor | wc -l)
+sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
+
+# Always chown webroot for better mounting
+chown -Rf nginx:nginx /usr/share/nginx/html
+
+# Start supervisord and services
+/usr/local/bin/supervisord -n -c /etc/supervisord.conf
--- image-build.sh
+++ image-build.sh
@@ -7,19 +7,15 @@ echo "Building image !"
 
 docker build . -t local/easyappointments:local
 
-echo "Saving image tar!"
-docker save local/easyappointments:local > nebua-easyappointments.tar
+#echo "Saving image tar!"
+#docker save local/easyappointments:local > nebua-easyappointments.tar
 
 # Import to microk8s
-echo "Importing image to microk8s!"
-sudo microk8s ctr image import nebua-easyappointments.tar
+#echo "Importing image to microk8s!"
+#sudo microk8s ctr image import nebua-easyappointments.tar
 
-echo "Remove File tar!"
-sudo rm -r nebua-easyappointments.tar
+#echo "Remove File tar!"
+#sudo rm -r nebua-easyappointments.tar
 echo "Done!"
 
-<<<<<<< HEAD
-docker image push nebua/easyappointments:latest
-=======
 #docker image push nebua/easyappointments:latest
->>>>>>> f9e68cbd5898bc562544af87e9d5e9e0125f6d61
