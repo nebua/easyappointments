a5c05ec2bc34342750c1000c8eec1284ab746c93
Angelo Correia <angelocorreia27@hotmail.com>
2022-04-21 11:09:26 -0100

    add autoloader

--- docker/run.sh
+++ docker/run.sh
@@ -7,6 +7,6 @@ bash ./docker/generateConfig.sh /var/www/public/config.php
 
 npm install 
 composer update
-composer install #--optimize-autoloader --no-dev
+composer install optimize-autoloader --no-dev
 
 /usr/bin/supervisord -c /etc/supervisord.conf
